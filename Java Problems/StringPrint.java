27c546f23cae82941e621bd4aee971daa2295290
Prajwal Pai <prajwalpai21@gmail.com>
2025-11-18 18:39:33 +0530

    Basic Java Problems

---
+++ Java Problems/StringPrint.java
@@ -0,0 +1,12 @@
+class StringPrint{
+	public static void main(String args[]){
+		String s1 = "Hello World";
+		System.out.println(s1);
+		
+		System.out.println("Length of String :"+s1+" is "+s1.length());
+		System.out.println("First value of "+s1+" is "+s1.charAt(0));
+		
+		char c1 = 'A';
+		System.out.println("c1 Value is "+c1);
+	}
+}
---
+++ Java Problems/Stringreverse.java
@@ -0,0 +1,8 @@
+class Stringreverse{
+	public static void main(String args[]){
+		String str = "Reverse This";
+		System.out.println("Original String : "+str);
+		for(int i=0; i != '\0';i++)
+			System.out.println(str.charAt(0));	
+	}
+}
